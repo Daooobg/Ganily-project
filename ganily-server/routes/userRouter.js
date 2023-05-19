d64c695c5ba8b195a95ec98898ece127b597160d
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-19 10:07:11 +0100

    feat: login route

--- ganily-server/routes/userRouter.js
+++ ganily-server/routes/userRouter.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const authController = require('../controllers/authController')
 
 router.route('/register').post(authController.register);
+router.route('/login').post(authController.login);
+
 
 module.exports = router;
