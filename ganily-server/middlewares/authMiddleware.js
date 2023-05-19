8466112b0393c517c111360f851e4d098277ecee
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-19 14:50:50 +0100

    fead: check for req.user

--- ganily-server/middlewares/authMiddleware.js
+++ ganily-server/middlewares/authMiddleware.js
@@ -24,6 +24,9 @@ exports.authentication = catchAsync(async (req, res, next) => {
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
+    if (!req.user) {
+      return next(new AppError('You are not login', 403, req.body));
+    }
     if (!roles.includes(req.user.role)) {
       return next(
         new AppError('You do not have permission to perform this action', 403)
