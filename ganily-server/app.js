8ee2a9da4dfdcae91106a4cf86289fb090743113
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-19 09:53:16 +0100

    feat: user route

--- ganily-server/app.js
+++ ganily-server/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 
+const userRouter = require('./routes/userRouter')
+
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
+
 
 module.exports = app;
