d74217a2d23cc94e464b132d46b7475400e6bc07
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-21 08:13:47 +0100

    fix: change res

--- ganily-server/controllers/cakeController.js
+++ ganily-server/controllers/cakeController.js
@@ -3,26 +3,40 @@ const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.create = catchAsync(async (req, res, next) => {
-  console.log('cake create');
   const data = await cakeService.create(req.body, req.user);
-  res.status(200).json(data);
+
+  res.status(201).json({
+    status: 'success',
+    data: data,
+  });
 });
 
 exports.getCakes = catchAsync(async (req, res, next) => {
-  //   const data = await cakeService.getCakes(req.body);
   const data = await cakeService.getCakes(req.query);
-  res.status(200).json(data);
+
+  res.status(200).json({
+    status: 'success',
+    result: data.length,
+    data: data,
+  });
 });
 
 exports.getOne = catchAsync(async (req, res, next) => {
   const data = await cakeService.getOne(req.params.slug);
 
-  res.status(200).json(data[0]);
+  res.status(200).json({
+    status: 'success',
+    data: data,
+  });
 });
 
 exports.updateOne = catchAsync(async (req, res, next) => {
   const data = await cakeService.updateOne(req.params.slug, req.body);
-  res.status(200).json(data);
+
+  res.status(200).json({
+    status: 'success',
+    data: data,
+  });
 });
 
 exports.deleteOne = catchAsync(async (req, res, next) => {
@@ -30,5 +44,9 @@ exports.deleteOne = catchAsync(async (req, res, next) => {
   if (result === null) {
     return res.status(404).json('Not found');
   }
-  res.status(204).json('Deleted');
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
