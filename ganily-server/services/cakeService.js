c09095e0a385d00c79b3f10e901de7234e8fb6ae
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-22 13:26:14 +0100

    feat: use factory service

--- ganily-server/services/cakeService.js
+++ ganily-server/services/cakeService.js
@@ -1,43 +1,8 @@
 const Cake = require('../models/cakeModel');
-const slug = require('slug');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/AppError');
-const APIFeatures = require('../utils/apiFeatures');
+const factoryService = require('./factoryService');
 
-exports.create = async (data, user) => {
-  data.ownerId = user._id;
-
-  return await Cake.create(data);
-};
-
-// exports.getCakes = async (data) => {
-//   return await Cake.find(data);
-// };
-
-exports.getCakes = async (queryData) => {
-  const feature = new APIFeatures(Cake.find(), queryData)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-    return await feature.query;
-};
-
-exports.getOne = async (data) => {
-  return await Cake.findOne({ slug: data });
-};
-
-exports.updateOne = async (slugData, data) => {
-  if (data.name) {
-    data.slug = slug(`${data.name}`, {
-      lower: true,
-    });
-  }
-  const cake = await Cake.findOneAndUpdate({ slug: slugData }, data, {
-    new: true,
-    runValidators: true,
-  });
-  return cake;
-};
-
-exports.deleteOne = (slugData) => Cake.findOneAndDelete({ slug: slugData });
+exports.createOne = factoryService.createOne(Cake);
+exports.getAll = factoryService.getAll(Cake);
+exports.getOne = factoryService.getOne(Cake);
+exports.updateOne = factoryService.updateOne(Cake);
+exports.deleteOne = factoryService.deleteOne(Cake);
