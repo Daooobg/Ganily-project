fc609d5783a1a95cd344d1f2f0f4855a293f4e09
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-22 13:27:37 +0100

    feat: use factory controller

--- ganily-server/controllers/cakeController.js
+++ ganily-server/controllers/cakeController.js
@@ -1,61 +1,8 @@
 const cakeService = require('../services/cakeService');
-const AppError = require('../utils/AppError');
-const catchAsync = require('../utils/catchAsync');
+const factoryController = require('./factoryController');
 
-exports.create = catchAsync(async (req, res, next) => {
-  const data = await cakeService.create(req.body, req.user);
-
-  res.status(201).json({
-    status: 'success',
-    data: data,
-  });
-});
-
-exports.getCakes = catchAsync(async (req, res, next) => {
-  const data = await cakeService.getCakes(req.query);
-
-  res.status(200).json({
-    status: 'success',
-    result: data.length,
-    data: data,
-  });
-});
-
-exports.getOne = catchAsync(async (req, res, next) => {
-  const data = await cakeService.getOne(req.params.slug);
-
-  if (!data) {
-    return next(new AppError(`No data found for: ${req.params.slug}`, 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: data,
-  });
-});
-
-exports.updateOne = catchAsync(async (req, res, next) => {
-  const data = await cakeService.updateOne(req.params.slug, req.body);
-
-  if (!data) {
-    return next(new AppError(`No data found for: ${req.params.slug}`, 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: data,
-  });
-});
-
-exports.deleteOne = catchAsync(async (req, res, next) => {
-  const data = await cakeService.deleteOne(req.params.slug);
-
-  if (!data) {
-    return next(new AppError(`No data found for: ${req.params.slug}`, 404));
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+exports.create = factoryController.createOne(cakeService);
+exports.getCakes = factoryController.getAll(cakeService);
+exports.getOne = factoryController.getOne(cakeService);
+exports.updateOne = factoryController.updateOne(cakeService);
+exports.deleteOne = factoryController.deleteOne(cakeService);
