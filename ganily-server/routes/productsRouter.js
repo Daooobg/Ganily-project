7343bc846c123ec9b148f570578173282e321a5a
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-22 14:30:30 +0100

    feat: candies route

--- ganily-server/routes/productsRouter.js
+++ ganily-server/routes/productsRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const cakeController = require('../controllers/cakeController');
+const candyController = require('../controllers/candyController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.route('/cakes').post(cakeController.create).get(cakeController.getCakes);
@@ -11,6 +12,19 @@ router
   .delete(
     authMiddleware.restrictTo('admin', 'owner'),
     cakeController.deleteOne
+  );
+
+router
+  .route('/candies')
+  .post(candyController.create)
+  .get(candyController.getCandy);
+router
+  .route('/candies/:slug')
+  .get(candyController.getOne)
+  .patch(authMiddleware.restrictTo('admin', 'owner'), candyController.updateOne)
+  .delete(
+    authMiddleware.restrictTo('admin', 'owner'),
+    candyController.deleteOne
   );
 
 module.exports = router;
