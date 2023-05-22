ac778a247d172d55eb16e26ebfb263f48eff9a17
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-22 09:49:06 +0100

    feat: handle token errors

--- ganily-server/controllers/errorController.js
+++ ganily-server/controllers/errorController.js
@@ -29,25 +29,40 @@ const handleValidationError = (err) => {
 const handleDuplicateFields = (err) => {
   // console.log(err.keyValue.name);
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-//   console.log(value);
+  //   console.log(value);
 
   const message = `Duplicate field value: (${value}). Please use another value!`;
   return new AppError(message, 400);
 };
 
+const handleTokenExpiredError = () =>
+  new AppError('Your token has expired. Please log in again!', 401);
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  console.log(err);
 
   if (err.name === 'ValidationError') {
     err = handleValidationError(err);
   }
   if (err.name === 'CastError') {
-    err = handleCastError(error);
+    err = handleCastError(err);
   }
 
   if (err.code === 11000) {
     err = handleDuplicateFields(err);
+  }
+
+  if (err.name === 'TokenExpiredError') {
+    err = handleTokenExpiredError();
+  }
+
+  if (err.name === 'JsonWebTokenError') {
+    err = handleJWTError();
   }
 
   sendError(err, res);
