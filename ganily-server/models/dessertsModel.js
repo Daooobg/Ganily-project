170efc972a3102d40e01701883f09cdc5a616613
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-22 15:20:12 +0100

    fix: regexp

--- ganily-server/models/dessertsModel.js
+++ ganily-server/models/dessertsModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slug = require('slug');
 
+const startUrl = /^(https?:\/)?\/.*/i;
+
 const dessertSchema = new mongoose.Schema({
   name: {
     type: String,
