aadbc31357ff00329c36a6e20c8b60646239ab5c
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-05-24 19:13:53 +0100

    feat: products page in router

--- ganily-app/src/App.js
+++ ganily-app/src/App.js
@@ -2,15 +2,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import RootLayout from './pages/Root';
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
+      { index: true, element: <HomePage /> },
       {
-        index: true,
-        element: <HomePage />,
+        path: 'products',
+        children: [{ index: true, element: <ProductsPage /> }],
       },
     ],
   },
